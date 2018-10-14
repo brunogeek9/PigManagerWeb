f881c185147feef1d3b8e500843053d52e74195f
dryel23 <adryel.juliao1@gmail.com>
2018-10-14 20:01:10 -0300

    template com o framework bootsfaces

--- src/main/java/bean/MatrizBean.java
+++ src/main/java/bean/MatrizBean.java
@@ -2,8 +2,12 @@ package bean;
 
 import java.util.Date;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.model.UploadedFile;
 
 @ManagedBean
 @ViewScoped
@@ -11,6 +15,15 @@ public class MatrizBean {
 
 	private Date data;
 
+	private UploadedFile file;
+	 
+    public UploadedFile getFile() {
+        return file;
+    }
+ 
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    } 
 	public void printData(){
 		System.out.println(data);
 	}
@@ -23,7 +36,12 @@ public class MatrizBean {
 		this.data = data;
 	}
 	
-	
+	 public void upload() {
+	        if(file != null) {
+	            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
+	            FacesContext.getCurrentInstance().addMessage(null, message);
+	        }
+	    }
 	
 	
 }
